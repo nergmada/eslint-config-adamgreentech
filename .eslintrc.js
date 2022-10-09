88ff280691fd2d8f643c57ef8b3caa33d06b351a
Adam Green <adam.green@kcl.ac.uk>
2022-10-09 17:22:55 +0100

    making more minor fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -157,7 +157,7 @@ module.exports = {
         // Basically new line if there's a chain of 3 or more calls
         "newline-per-chained-call": "error",
         // Always new line object declarations
-        "object-curly-newline": ["error", "always"],
+        "object-curly-newline": ["error", "consistent"],
         // Don't catch just to throw, just throw through
         // Might be the case that handling needed to be implemented that wasn't
         "no-useless-catch": "warn",
